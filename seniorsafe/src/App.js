434c7b3bb8962c93231f68ccd747a6877f623e83
Ziyao Piong <ziyaopiong@Ziyaos-MacBook-Pro.local>
2024-08-24 18:28:05 +1000

    added footer

--- seniorsafe/src/App.js
+++ seniorsafe/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/ui/Navbar/Navbar';
+import Footer from './pages/Home/Footer';
 import LandingPage from './pages/Home/LandingSection';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       {/* <LandingPage /> */}
+      <Footer />
     </div>
   );
 }
