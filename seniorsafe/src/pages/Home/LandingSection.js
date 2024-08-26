8236d5a7cd0321ce5540072b35524eff12b57101
Ziyao Piong <ziyaopiong@Ziyaos-MacBook-Pro.local>
2024-08-26 21:48:04 +1000

    edited landing section

--- seniorsafe/src/pages/Home/LandingSection.js
+++ seniorsafe/src/pages/Home/LandingSection.js
@@ -1,17 +1,6 @@
 // src/LandingPage.js
 // import './LandingSection.css';
 import { Container, Row, Col, Button } from 'react-bootstrap';
-// const LandingPage = () => {
-//   return (
-//     <div className="landing-page">
-//       <div className="content">
-//         <h1>Welcome to Our Website</h1>
-//         <p>Your journey to excellence starts here.</p>
-//         <button className="cta-button">Get Started</button>
-//       </div>
-//     </div>
-//   );
-// };
 
 function LandingSection() {
   return (
