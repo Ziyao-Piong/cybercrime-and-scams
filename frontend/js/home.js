40ad040960d87fb7dc395c01751ceeca4f3b012f
prashansachadha-hub <pcha0092@student.monash.edu>
2024-09-16 19:22:31 +1000

    facts added, and some changes in the js file

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -2,7 +2,17 @@ const quotes = [
     "Australians lost a record amount of more than $2.7 billion to scams in 2023.",
     "Have you been scammed?",
     "Seniors are often targeted by scammers due to their perceived vulnerability.",
-    "Over 70% of scam victims aged 65 and older report financial losses."
+    "Over 70% of scam victims aged 65 and older report financial losses.",
+    "Australians Lost Over $3.1 Billion to Scams in 2022",
+    "Senior Australians (65+) Reported Losing $120.7 Million in 2022",
+    "Phishing Scams Accounted for $23.8 Million in Losses Last Year",
+    "Scam Losses Increased by 80% in a Single Year",
+    "Remote Access Scams Cost Australians Over $30 Million in 2022",
+    "Investment Scams Caused a Staggering $1.5 Billion in Losses",
+    "Phone Call Scams Resulted in $18.6 Million in Losses in 2022",
+    "Fake Online Shopping Scams Scammed Australians Out of $8.6 Million",
+    "Seniors Are a Prime Target for Scammers Due to Their Trusting Nature",
+    "Government Impersonation Scams Are on the Rise Among Seniors"
 ];
 
 let currentQuoteIndex = 0;
